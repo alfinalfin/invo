091b4b0ae6b611033b116ae5d6e13bea508368c3
Alfin Faisal <alfin9147@gmail.com>
2026-04-18 13:58:51 +0530

    bind explicitly to 0.0.0.0 to fix render health checks

--- src/server.js
+++ src/server.js
@@ -3,6 +3,6 @@ import { logger } from "./utils/logger.js";
 
 const port = Number(process.env.PORT) || 5000;
 
-app.listen(port, () => {
+app.listen(port, "0.0.0.0", () => {
   logger.info("Server started.", { port });
 });
